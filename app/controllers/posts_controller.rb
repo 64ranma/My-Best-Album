8b6110dedc2b345cc63ac0f0262d2e2cfbcdbb91
EC2 Default User <ec2-user@ip-172-31-45-79.ap-northeast-1.compute.internal>
2024-11-29 06:11:41 +0000

    flash

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,10 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
+      flash[:notice] = "投稿に成功しました。"
       redirect_to posts_path
     else
+      flash.now[:alert] = "投稿に失敗しました。"
       render :new
     end
   end
@@ -42,6 +44,7 @@ class PostsController < ApplicationController
   def destroy
     post = Post.find(params[:id])
     post.destroy
+    flash[:alert] = "記事を削除しました。"
     redirect_to posts_path
   end
 end
